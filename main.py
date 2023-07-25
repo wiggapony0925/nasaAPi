858320f8a95c9a6de8ef746ed77a7ecb034777a7
wiggapony0925 <ninjeff06@gmail.com>
2023-07-25 10:18:13 -0400

    added key and made request

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
 import requests
 import os
+from dotenv import load_dotenv
 
+# load the env
+load_dotenv()
+
+key = os.getenv("API_KEY")
 url = 'https://api.nase.gov/planetary/apod'
+
+parameters = {
+    'api key': key,
+    'data': '2022-2-22',
+    'start_date': "2022-2-20",
+    'end_date': '2022-2-22',
+    'count': 5,
+    'thumbs': True,
+}
+
+response = requests.get(url, params=parameters)
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print(f"request failed with the status code: {response.status_code}")
